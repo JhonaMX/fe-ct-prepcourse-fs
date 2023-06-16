9b65cf82062461f6f068767383abfb58a13a73f7
Jhonatan <jhonaslunay@gmail.com>
2023-06-16 17:35:58 -0600

    Extra relleno

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -64,14 +64,14 @@ function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
-   let newArre = [];
+   let newArre = []X; //X
    for (var i = 0; i < arrayOfStrings.length; i++){
       if (arrayOfStrings[i][0] == "a") {
          newArre.push(arrayOfStrings[i])
       }
-   }
+   } //truky
    return newArre;
-}
+}//////////AHUEVO PERRO!
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
